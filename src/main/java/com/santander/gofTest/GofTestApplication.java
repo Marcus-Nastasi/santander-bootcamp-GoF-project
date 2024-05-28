2f8cbc4e295503905a0160724fb7895b88902bec
rolemberg <vinnie.nsts@gmail.com>
2024-05-28 12:39:13 -0300

    first commit

---
+++ src/main/java/com/santander/gofTest/GofTestApplication.java
@@ -0,0 +1,13 @@
+package com.santander.gofTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santander/gofTest/GofTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.gofTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
