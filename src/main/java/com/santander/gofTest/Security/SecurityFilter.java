e31650caad390263163e7f7c704cae385e7dcecd
rolemberg <vinnie.nsts@gmail.com>
2024-05-30 12:46:11 -0300

    created security filter class;

--- src/main/java/com/santander/gofTest/Security/SecurityFilter.java
+++ src/main/java/com/santander/gofTest/Security/SecurityFilter.java
@@ -1,9 +1,17 @@
 package com.santander.gofTest.Security;
 
+import com.santander.gofTest.Repository.UserRepo;
+import com.santander.gofTest.Service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,9 +20,31 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UserRepo userRepo;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recoverToken(request);
 
+        if (token != null) {
+            var email = tokenService.validate(token);
+            UserDetails u = userRepo.findByEmail(email);
+
+            var auth = new UsernamePasswordAuthenticationToken(u, null, u.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Bean
+    private String recoverToken(HttpServletRequest req) {
+        var header = req.getHeader("Authorization");
+        if (header == null) return null;
+        return header.replace("Bearer ", "");
     }
 }
 
